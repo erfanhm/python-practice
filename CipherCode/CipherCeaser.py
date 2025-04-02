137be3b9cf6e75c0d7ec34fb4d0c5edc0848a223
erfanhm <erfanhamidi077@gmail.com>
2025-04-02 14:16:24 -0400

    Updated

--- CipherCode/CipherCeaser.py
+++ CipherCode/CipherCeaser.py
@@ -1,4 +1,7 @@
+import art
+print(art.logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 def caesar(original_text, shift_amount, encode_or_decode):
     if encode_or_decode == "decode":
         shift_amount *= -1
@@ -10,18 +13,23 @@ def caesar(original_text, shift_amount, encode_or_decode):
             output_text += alphabet[shifted_position]
         else:
             output_text += letter
-    print(f"Here is the result: {output_text}")
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-if direction == "encode" or direction == "decode":
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+condition=True
+while condition:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-    caesar(original_text=text,shift_amount=shift,encode_or_decode=direction)
-else:
-    print("Please type 'encode' or 'decode'.")
-
-
-
-
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    decision=input("Do you want to continue? (yes/no)\n").lower()
+    if decision == "yes":
+        condition = True
+    elif decision == "no":
+        condition = False
+        print("Thank you for having us :) ")
+    else:
+        print("please type 'y' or 'n'")
+        condition = True
 
 
 
